c7e897ef78fa11e1bd90a7652663ab361ca66bb5
phuckhanh2005 <khanhskyler1233@gmail.com>
2026-05-25 11:29:33 +0700

    second commit

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,16 @@
 import express from "express";
-
+import homeController from "../controllers/homeController"
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) =>
-    {
-        return res.send('nha lu dut thui ')
-    });
+    router.get('/', homeController.getHomePage );
+    router.get('/about',homeController.getAboutPage );
+    
+
+    //rest api
+
+
+
     return app.use("/", router);
 }
 
